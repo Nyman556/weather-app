ed1b6542e5442bb9b079497e93b207334b84bda1
Johan Nyman <johan.nyman97@gmail.com>
2023-06-05 19:23:13 +0200

    comments for data structure for fetch

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 feather.replace();
+
+// example https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key} || https://api.openweathermap.org/data/2.5/weather?q=London
+// parametrar
+// &units=metric
+// &appid={API key}
+
+// data -> data returns are unix Timestamps
+
+// data.dt: 1645888976,
+// data.sunrise: 1645853361,
+// data.sunset: 1645891727,
+// data.temp: 22,
+// data.feels_like: 18,
+// data.humidity: 64,
+// data.uvi: 0.06,
+
+// data.visibility
+// data.wind_speed
+
+// data.wheater
+// data.wheater.main = "Clear"
+// data.wheater.description = "clear sky"
+// data.wheater.icon = "01d"
+// https://openweathermap.org/weather-conditions -> icon-list -> URL is https://openweathermap.org/img/wn/10d@2x.png
