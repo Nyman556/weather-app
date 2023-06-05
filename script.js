40d61407df9d179bfc11717ae495c69697e56fe8
Johan Nyman <johan.nyman97@gmail.com>
2023-06-05 23:17:42 +0200

    render weekday, fixed clock

--- script.js
+++ script.js
@@ -10,18 +10,32 @@ feather.replace();
 // https://openweathermap.org/weather-conditions -> icon-list -> URL is https://openweathermap.org/img/wn/10d@2x.png
 
 // DOM objects
-const miscData = document.querySelectorAll(".misc-data");
-const setData = document.querySelectorAll(".set-data");
+const currentTime = document.getElementById("time");
+const day = document.getElementById("day");
 const mainWeaterImg = document.getElementById("main-weather-img");
 const mainDayDegree = document.getElementById("main-day-degree");
 const mainDayInfo = document.getElementById("main-day-info");
+const miscData = document.querySelectorAll(".misc-data");
+const setData = document.querySelectorAll(".set-data");
 
 // variables
-let location1 = "Växjö";
+let Weekdays = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+let city = "Växjö";
 let key = "0624fd04c06bf97df66d95012813bc7b";
+let currentDate = new Date();
+let weekday = currentDate.getDay();
+console.log(Weekdays[weekday]);
 
 fetch(
-	`http://api.openweathermap.org/data/2.5/weather?q=${location1}&appid=${key}&units=metric`
+	`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
 )
 	.then((response) => response.json())
 	.then((wheaterData) => {
@@ -36,12 +50,12 @@ function renderWheaterData(data) {
 	miscData[0].innerHTML = data.main.pressure + " hPa";
 	miscData[1].innerHTML = data.wind.speed + " m/s";
 	miscData[2].innerHTML = data.main.humidity + "%";
-	miscData[3].innerHTML = Number(data.visibility) / 1000 + " km";
+	miscData[3].innerHTML = data.visibility / 1000 + " km";
 	description.innerHTML = data.weather[0].main;
 	subDesciption.innerHTML = data.weather[0].description;
 	subDesciption.classList.add("sub-info");
 	mainDayInfo.append(description, subDesciption);
-	let d = new Date(data.dt * 1000);
+	day.innerHTML = Weekdays[weekday];
 }
 // Eftersom gratisvarianten av OpenWeather inte ger sol-upgång/nedgång så hämtar jag det från en annan API
 fetch(
@@ -53,10 +67,16 @@ fetch(
 		setData[1].innerHTML = setDataFetched.results.sunset;
 	});
 
-let a;
-let time;
 setInterval(() => {
 	clock = new Date();
-	time = clock.getHours() + ":" + clock.getMinutes();
-	document.getElementById("time").innerHTML = time;
+	hour = clock.getHours();
+	min = clock.getMinutes();
+	if (hour < 10) {
+		hour = "0" + clock.getHours();
+	}
+	if (min < 10) {
+		min = "0" + clock.getMinutes();
+	}
+	let time = hour + ":" + min;
+	currentTime.innerHTML = time;
 }, 1000);
