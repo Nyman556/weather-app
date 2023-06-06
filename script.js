f43fd3f7b65a8c61694c2df207684b5eef47202e
Johan Nyman <johan.nyman97@gmail.com>
2023-06-06 13:56:02 +0200

    getDate function, cleanup, comments

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
+// refreshes Icons
 feather.replace();
-// data -> data returns are unix Timestamps
-
-// data.dt: 1645888976,
-// data.sunrise: 1645853361,
-// data.sunset: 1645891727,
-
-// data.wheater
-// data.wheater.icon = "01d"
-// https://openweathermap.org/weather-conditions -> icon-list -> URL is https://openweathermap.org/img/wn/10d@2x.png
 
 // DOM objects
+const timestamp = document.getElementById("update-timestamp");
 const currentTime = document.getElementById("time");
 const day = document.getElementById("day");
 const mainWeaterImg = document.getElementById("main-weather-img");
 const mainDayDegree = document.getElementById("main-day-degree");
+const mainDaymaxMix = document.getElementById("max-min");
 const mainDayInfo = document.getElementById("main-day-info");
 const miscData = document.querySelectorAll(".misc-data");
 const setData = document.querySelectorAll(".set-data");
@@ -28,34 +22,49 @@ let Weekdays = [
 	"Friday",
 	"Saturday",
 ];
-let city = "Växjö";
 let key = "0624fd04c06bf97df66d95012813bc7b";
 let currentDate = new Date();
 let weekday = currentDate.getDay();
-console.log(Weekdays[weekday]);
+let date;
+let clockTime;
 
 fetch(
-	`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+	`http://api.openweathermap.org/data/2.5/weather?q=Växjö&appid=${key}&units=metric`
 )
 	.then((response) => response.json())
-	.then((wheaterData) => {
-		renderWheaterData(wheaterData);
+	.then((weatherData) => {
+		renderWheaterData(weatherData);
 	});
 
 function renderWheaterData(data) {
 	mainWeaterImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-	mainDayDegree.innerHTML = Math.round(data.main.temp) + "°";
+	mainDayDegree.innerHTML = data.main.temp + "°";
+
+	//Description section
 	let description = document.createElement("p");
 	let subDesciption = document.createElement("p");
-	miscData[0].innerHTML = data.main.pressure + " hPa";
-	miscData[1].innerHTML = data.wind.speed + " m/s";
-	miscData[2].innerHTML = data.main.humidity + "%";
-	miscData[3].innerHTML = data.visibility / 1000 + " km";
 	description.innerHTML = data.weather[0].main;
 	subDesciption.innerHTML = data.weather[0].description;
 	subDesciption.classList.add("sub-info");
 	mainDayInfo.append(description, subDesciption);
-	day.innerHTML = Weekdays[weekday];
+
+	timestamp.innerHTML = "Last Updated: " + getDate();
+	mainDaymaxMix.innerHTML =
+		Math.round(data.main.temp_max) +
+		"°" +
+		" | " +
+		Math.round(data.main.temp_min) +
+		"°";
+
+	// gets clock and date once here then in interval
+	currentTime.innerHTML = getDate();
+	day.innerHTML = getDate(1) + " | " + Weekdays[weekday];
+
+	// array of DOM cards for [pressure, wind, humidity, visiblity]
+	miscData[0].innerHTML = data.main.pressure + " hPa";
+	miscData[1].innerHTML = data.wind.speed + " m/s";
+	miscData[2].innerHTML = data.main.humidity + "%";
+	miscData[3].innerHTML = data.visibility / 1000 + " km";
 }
 // Eftersom gratisvarianten av OpenWeather inte ger sol-upgång/nedgång så hämtar jag det från en annan API
 fetch(
@@ -67,16 +76,35 @@ fetch(
 		setData[1].innerHTML = setDataFetched.results.sunset;
 	});
 
+// function to get date or clock depending on if there is an input
+function getDate(x) {
+	let date = new Date();
+	if (x) {
+		let year = date.getFullYear();
+		let month = date.getMonth();
+		let day = date.getDate();
+		if (month < 10) {
+			month = "0" + date.getMonth();
+		}
+		if (day < 10) {
+			day = "0" + date.getDate();
+		}
+		return (date = year + "-" + month + "-" + day);
+	} else {
+		let hour = date.getHours();
+		let min = date.getMinutes();
+		if (hour < 10) {
+			hour = "0" + date.getHours();
+		}
+		if (min < 10) {
+			min = "0" + date.getMinutes();
+		}
+		return (clockTime = hour + ":" + min);
+	}
+}
+
+// Intervals
 setInterval(() => {
-	clock = new Date();
-	hour = clock.getHours();
-	min = clock.getMinutes();
-	if (hour < 10) {
-		hour = "0" + clock.getHours();
-	}
-	if (min < 10) {
-		min = "0" + clock.getMinutes();
-	}
-	let time = hour + ":" + min;
-	currentTime.innerHTML = time;
+	currentTime.innerHTML = getDate();
+	day.innerHTML = getDate(1) + " | " + Weekdays[weekday];
 }, 1000);
